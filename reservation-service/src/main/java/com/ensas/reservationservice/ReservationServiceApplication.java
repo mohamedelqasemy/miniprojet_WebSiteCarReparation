31cf801565be760ca00e86fd82c81fda3d2ae4a0
mohamedelqasemy <melqasemy2003@gmail.com>
2025-02-13 11:56:23 +0100

    add commandlinerunner to add some reservations to db

--- reservation-service/src/main/java/com/ensas/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/ensas/reservationservice/ReservationServiceApplication.java
@@ -1,8 +1,18 @@
 package com.ensas.reservationservice;
 
+import com.ensas.reservationservice.Repositories.ReservationRepository;
+import com.ensas.reservationservice.entities.Reservation;
+import com.ensas.reservationservice.enums.EnumStatus;
+import com.ensas.reservationservice.services.ReservationService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +20,30 @@ public class ReservationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ReservationRepository reservationRepository) {
+        return args -> {
+            List<Reservation> reservations = List.of(
+                    Reservation.builder()
+                            .date(new Date())
+                            .status(EnumStatus.Pending)
+                            .serviceId(1L)
+                            .carId(1L)
+                            .userId(1L)
+                            .build()
+                    ,
+                    Reservation.builder()
+                            .date(new Date())
+                            .status(EnumStatus.Pending)
+                            .serviceId(1L)
+                            .carId(1L)
+                            .userId(2L)
+                            .build()
+            );
+            reservationRepository.saveAll(reservations);
+        };
     }
 
 }
--- reservation-service/src/main/java/com/ensas/reservationservice/entities/Reservation.java
+++ reservation-service/src/main/java/com/ensas/reservationservice/entities/Reservation.java
@@ -12,6 +12,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
