4ae866039b41e62e58359daa6d44f77a6de66459
mohamedelqasemy <melqasemy2003@gmail.com>
2025-05-08 16:02:53 +0100

    refactor(garage-service): ajout de 12 garage avec lors adress il faut respecter lor de ajout d'un garage:rue,ville,pays

--- garage-service/src/main/java/com/ensas/garageservice/GarageServiceApplication.java
+++ garage-service/src/main/java/com/ensas/garageservice/GarageServiceApplication.java
@@ -31,11 +31,29 @@ public class GarageServiceApplication {
 
             Random random = new Random();
 
+            // Ajoutez des points de repère et des informations supplémentaires pour chaque ville
+            List<String> adresses = Arrays.asList(
+                    "Rue Mohammed V, Agadir, Maroc",
+                    "Avenue Hassan II, Marrakech, Maroc",
+                    "Avenue des Nations Unies, Safi, Maroc",
+                    "Boulevard Moulay Youssef, Rabat, Maroc",
+                    "Rue de la Liberté, Tanger, Maroc",
+                    "Avenue Hassan II, Casablanca, Maroc",
+                    "Avenue Mohammed V, Tinghir, Maroc",
+                    "Avenue Hassan II, Zagora, Maroc",
+                    "Boulevard Mohammed V, Ouarzazate, Maroc",
+                    "Avenue Hassan II, Tiznit, Maroc",
+                    "Avenue Mohamed V, Beni Mellal, Maroc",
+                    "Rue Al Massira, Azilal, Maroc"
+            );
+
             for (int i = 0; i < villes.size(); i++) {
                 String ville = villes.get(i);
+                String adresse = adresses.get(i); // Utiliser une adresse plus complète
+
                 Garage garage = Garage.builder()
                         .nom("Garage " + ville)
-                        .adresse("Rue principale, " + ville)
+                        .adresse(adresse)
                         .telephone("06" + (random.nextInt(90000000) + 10000000))
                         .note(random.nextInt(6)) // note entre 0 et 5
                         .OuvertureDate(new Date(System.currentTimeMillis() - 1000000000L)) // ouverture dans le passé
