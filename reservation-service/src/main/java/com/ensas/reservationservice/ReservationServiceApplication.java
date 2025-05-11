62f9cbb1733d86257fa7f801e6aad3f1831394c7
Otmane Karym <109726485+otmaneDev@users.noreply.github.com>
2025-05-11 19:13:16 +0100

    modif(reservation): fixing the application class with new bean

--- reservation-service/src/main/java/com/ensas/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/ensas/reservationservice/ReservationServiceApplication.java
@@ -28,12 +28,12 @@ public class ReservationServiceApplication {
     CommandLineRunner commandLineRunner(ReservationService reservationService) {
         return args -> {
             List<ReservationRequest> reservationRequests = List.of(
-                    new ReservationRequest("Toyota", "Yaris", "Essence", 120000f, 1L, 1L, List.of(1L, 2L), new Date()),
-                    new ReservationRequest("Peugeot", "208", "Diesel", 80000f, 1L, 2L, List.of(2L), new Date()),
-                    new ReservationRequest("Renault", "Clio", "Hybride", 50000f, 1L, 3L, List.of(3L, 4L), new Date()),
-                    new ReservationRequest("Dacia", "Logan", "Essence", 60000f, 1L, 4L, List.of(4L), new Date()),
-                    new ReservationRequest("Hyundai", "i10", "Diesel", 75000f, 1L, 2L, List.of(5L), new Date()),
-                    new ReservationRequest("Ford", "Fiesta", "Essence", 90000f, 1L, 4L, List.of(6L), new Date())
+                    new ReservationRequest("Toyota", "Yaris", "Essence", 120000f,"d42234", 1L, 1L, List.of(1L, 2L), new Date()),
+                    new ReservationRequest("Peugeot", "208", "Diesel", 80000f,"d42234", 1L, 2L, List.of(2L), new Date()),
+                    new ReservationRequest("Renault", "Clio", "Hybride", 50000f,"d42234", 1L, 3L, List.of(3L, 4L), new Date()),
+                    new ReservationRequest("Dacia", "Logan", "Essence", 60000f,"d42234", 1L, 4L, List.of(4L), new Date()),
+                    new ReservationRequest("Hyundai", "i10", "Diesel", 75000f,"d42234", 1L, 2L, List.of(5L), new Date()),
+                    new ReservationRequest("Ford", "Fiesta", "Essence", 90000f,"d42234", 1L, 4L, List.of(6L), new Date())
             );
 
 
